d1ef10125bdd3b56d7bc7af73f109244f0ff803e
Oleksii Stremetskyi <stremetskyi.o@gmail.com>
2019-04-16 22:03:06 +0300

    Implemented model from 'End to End Learning for Self-Driving Cars'. Describe dataset better.

--- model.py
+++ model.py
@@ -1,17 +1,28 @@
-from keras import Input, Model
-from keras.applications import Xception
+import numpy as np
+from keras import Sequential
 from keras.callbacks import ModelCheckpoint
-from keras.layers import Dense, Cropping2D
+from keras.layers import Dense, Cropping2D, Lambda, Conv2D, Flatten
 from math import ceil
 
 from dataset import Dataset
+
+
+def resize(img):
+    dsize = (66, 200)
+    import tensorflow as tf
+    return tf.image.resize_images(img, dsize)
+
 
 if __name__ == '__main__':
     # Load and prepare dataset
     ds_shape = (160, 320, 3)
     dataset = Dataset('data')
     dataset.load()
-    print('Dataset size =', len(dataset.imgs), 'shape =', ds_shape)
+    print('Dataset: size =', len(dataset.imgs), 'shape =', ds_shape)
+    labels = np.array(dataset.labels)
+    straight_label_count = np.count_nonzero(np.abs(labels) <= 0.03)
+    print('Labels: mean =', np.mean(labels), 'std =', np.std(labels), 'straight ratio =',
+          straight_label_count / labels.size)
     dataset.augment()
     print('Dataset size after augmentation =', len(dataset.imgs))
     dataset.validation_split(0.2)
@@ -19,25 +30,30 @@ if __name__ == '__main__':
     print('Validation size=', len(dataset.x_valid))
 
     # Define the model
-    model_input = Input(shape=ds_shape)
-    model_input_crop = Cropping2D(cropping=((65, 24), (0, 0)))(model_input)
-
-    xception = Xception(include_top=False, weights=None, pooling='avg')
-
-    model_output = xception(model_input_crop)
-    model_output = Dense(1)(model_output)
-
-    model = Model(model_input, model_output)
+    model = Sequential()
+    model.add(Cropping2D(cropping=((65, 24), (0, 0)), input_shape=ds_shape))
+    model.add(Lambda(resize))
+    model.add(Lambda(lambda x: x / 128 - 1))
+    model.add(Conv2D(24, 5, strides=2, activation='relu'))
+    model.add(Conv2D(36, 5, strides=2, activation='relu'))
+    model.add(Conv2D(48, 5, strides=2, activation='relu'))
+    model.add(Conv2D(64, 3, activation='relu'))
+    model.add(Conv2D(64, 3, activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1))
     model.summary()
 
     # Train the model
-    batch_size = 64
+    batch_size = 32
     checkpoint = ModelCheckpoint('model.h5', verbose=1, save_best_only=True)
 
     model.compile('adam', 'mse')
     model.fit_generator(dataset.train_set(batch_size),
                         steps_per_epoch=ceil(len(dataset.x_train) / batch_size),
-                        epochs=3,
+                        epochs=8,
                         callbacks=[checkpoint],
                         validation_data=dataset.valid_set(batch_size),
                         validation_steps=ceil(len(dataset.x_valid) / batch_size))
