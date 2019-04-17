90fb0dced032257585c7f7c67231989ff9d1b352
Oleksii Stremetskyi <stremetskyi.o@gmail.com>
2019-04-17 20:52:55 +0300

    Increased driving speed.

--- drive.py
+++ drive.py
@@ -44,7 +44,7 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 9
+set_speed = 20
 controller.set_desired(set_speed)
 
 
@@ -62,6 +62,12 @@ def telemetry(sid, data):
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+
+        if float(speed) >= 15:
+            if steering_angle < -0.1:
+                steering_angle -= 0.05
+            elif steering_angle > 0.1:
+                steering_angle += 0.05
 
         throttle = controller.update(float(speed))
 
